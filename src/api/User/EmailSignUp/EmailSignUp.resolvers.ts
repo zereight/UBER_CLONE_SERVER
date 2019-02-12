00accd153e3e1fe9d6f9baedec7912de06225ca1
zereight <42544600+zereight@users.noreply.github.com>
2019-02-12 15:06:38 +0900

    17. EmailSignUp

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,42 @@
+import { Resolvers } from '../../../resolvers';
+import { EmailSignUpMutationArgs } from 'src/types';
+import { EmailSignUpResponse } from '../../../types';
+import User from 'src/entities/User';
+
+const resolvers: Resolvers= {
+    Mutation: {
+        EmailSignUp: async (_, args: EmailSignUpMutationArgs): Promise<EmailSignUpResponse> => {
+
+            const {email} = args;
+
+            try {
+                const existingUser = await User.findOne({email});
+
+                if(existingUser){
+                    return {
+                        ok: false,
+                        error: "You should log in instead.",
+                        token: null
+                    }
+                } else {
+                    const newUser = await User.create({...args}).save();
+                    return {
+                        ok: true,
+                        error: null,
+                        token: "Coming soon"
+                    };
+                }
+
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message,
+                    token: null
+                };
+            }
+
+        }
+    }
+};
+
+export default resolvers;
