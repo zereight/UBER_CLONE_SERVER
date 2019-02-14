613247af5448dc53fa7267d50eb7220403ec156c
zereight <42544600+zereight@users.noreply.github.com>
2019-02-14 20:55:03 +0900

    20. How authenticate JWT?
    주어진 JWT를 해석하는 함수를 만들고
     middleware에 추가하였다.
    JWT정보는 graphql playground에서
    직접 주었고
    나중엔 React를 이용하여 줄것이다.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import logger from "morgan";
 import helmet from "helmet";
 
 import schema from "./schema"
+import decodeJWT from './utils/decodeJWT';
 
 class App{
     public app : GraphQLServer;
@@ -21,7 +22,29 @@ class App{
         this.app.express.use(cors());
         this.app.express.use(logger("dev"));
         this.app.express.use(helmet());
+        this.app.express.use(this.jwt);
     }
+
+    //왜 jwt는 jwt()로 안썼는가? => 그냥 함수명만 쓰게 되면 express.use() 가 실행된 후 실행되나 ()를 쓰면 그 자리에서 실행되고 express.use()가 실행된다.
+
+    private jwt = async (request, response, next):Promise<void> => {
+        //request가 무엇을 갖고 있는지 확인하고
+        //그에 대한 Response가 정의되어 있다면 그것을 전송하고
+        //request가 아무것도 갖고 있지 않으면 next로 다음 미들웨어가 실행됨.
+        const token = request.get("X-JWT");
+        //지금 상황에서는 graphql playground에서
+        //내가 직접 FacebookConnect로 mutation요청 했을때 발급받은 token을
+        //페이지및 하단 http header에 
+        //{"X-JWT": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTUwMTQ0NzAxfQ.dT8t7C_FjHxADTYrTjizoETf3Wr2MIM1yybWRHCRSJQ"}
+        //이런식으로 넣어줄거임. 그러면 request로 이게 전달되고 X-JWT를 get해서 token을 읽어올거임
+        //http header에 적는 순간 실시간으로 등록되어
+        //그 토큰을 입력 했을 때 그 토큰의 user정보가 콘솔창에 출력될거임.
+        if(token){
+            const user = await decodeJWT(token);
+            console.log(user);
+        }
+        next();
+    };
 }
 
 export default new App().app;
