a3c24e5a0404ecea2df6b897090171583bd48df0
zereight <42544600+zereight@users.noreply.github.com>
2019-02-17 13:40:01 +0900

    23. Checking Resolver using curry syntax.
    앞서 GetMyProfile에서 우리는 user가 존재 하지 않아도 별다른 error없이 user:null로 출력되도록 놔두었다.
    
    if(context.req.user)로 체크해도 되지만
    새로운 문법인 curry로 만들어 보았다.
    
    const a = x => y => x+y
    const b = (x)(y) => x+y
    위 둘은 비슷하다고 할 수 있다.
    
    그래서 GetMyProfile의 resolver에서
    user를 반환하는 함수를 첫번째 인자로 넘겨주면, privateResolver가 받아서 첫번째 인자로 resolverFunction이름으로 그걸 쓰고 2번째 인자는 이미 정해진 비동기함수를 써준다.
    
    그럼 privateResolver안에서는 두번째 인자가 parent,args,context,info를 받았으므로 그걸 활용해 user여부를 체크하고, 통과하면 resolverFunction으로 user를 출력하는 작업을 하는 것이 목적이다.
    
    privateResolver에는 인자로 함수 1개를 넘겨줬고 2번째인자는 정해져 있다고 쳐도 2번째 인자의 인수들은 어떻게 집어넣었냐고 할 수 있지만, resolver는 실행할 때 마다 parent, args, context, info 를 다 전달하기 때문에 privateResolver에서 넘어온 것을 감지하고 받은것이다.

--- src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
+++ src/api/User/GetMyProfile/GetMyProfile.resolvers.ts
@@ -1,18 +1,21 @@
 import { Resolvers } from '../../../resolvers';
 import { GetMyProfileResponse } from '../../../types';
+import privateResolver from '../../../utils/privateResolver';
 
 const resolvers : Resolvers = {
     Query: {
-        GetMyProfile: async (parent, args, context) : Promise<GetMyProfileResponse> => {
-            const user = context.req.user;
-    
-            return {
-                ok: true,
-                error:null,
-                user
-            };
-    
-        }
+        GetMyProfile: privateResolver( //함수 1개를 넘겨줌.
+                async (parent, args, context) : Promise<GetMyProfileResponse> => {
+                const user = context.req.user;
+        
+                return {
+                    ok: true,
+                    error:null,
+                    user
+                };
+        
+            }
+        )
     }
 }
 
---
+++ src/utils/privateResolver.ts
@@ -0,0 +1,15 @@
+const privateResolver = resolverFunction => //curry방식을 이용. 그냥 x => y => x+y 랑 (x)(y) => x+y랑 비슷하다고 생각하면됨.
+async (parent, args, context, info) => {
+
+    //2번째 인수인 비동기함수의 인수로 http헤더(X-JWT)로 넘어온 토큰이 있는지 검사.
+    if(!context.req.user) {
+        throw new Error("No JWT. I refuse to proceed")
+    }
+
+    //아래 resolverFunction이 GetMyProfile.resolver에서 인수로 넘겨진 함수이다.
+    const resolved = await resolverFunction(parent, args, context, info);
+    return resolved;
+
+};
+
+export default privateResolver;
