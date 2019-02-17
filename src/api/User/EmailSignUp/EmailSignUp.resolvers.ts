fca9273d73856c2eb3446c49581ae811e7d27129
zereight <42544600+zereight@users.noreply.github.com>
2019-02-18 02:02:08 +0900

    + 24. EmailVerification
    지금 만들고 있는 회원가입 구조는
    폰인증하고 그 후에 이메일로 로그인하는 것을 적용중이다.
    
    그래서 우리는 email로 회원등록을 할때 StartPhoneVerification에서 그 폰에 관한 key를 생성해 주고  문자를 보낼
     것이고, CompeletPhoneVerification에서 해당 key를 가진 phoneVerification이 존재하는지 체크 후 .save()로 db에 저장해줄 것이다.
    
    이때 EmailSignUp에서 해당 폰번호와 검증여부가 true인 Verification을 찾아서 없으면 Email로 폰인증하라 하고,
    있으면 EmailVerification을 만들고 (아직 .save()로 db에 저장하진 않음)
    email로 코드를 보낸다.
    
    다음 시간엔 보낸 코드or링크로 검증하고 검증 되었으면 emailVerification을 .save()로 저장해서 회원 인증을 끝마칠 것이다.

--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -4,6 +4,7 @@ import { EmailSignUpResponse } from '../../../types';
 import User from '../../../entities/User';
 import createJWT from '../../../utils/createJWT';
 import { transporter, mailOption } from '../../../utils/sendEmail';
+import Verification from '../../../entities/Verification';
 
 const resolvers: Resolvers= {
     Mutation: {
@@ -21,23 +22,40 @@ const resolvers: Resolvers= {
                         token: null
                     }
                 } else {
-                    const user = await User.create({...args}).save();
-                    if(user.email){
-                        mailOption.to = user.email;
-                        mailOption.subject = "Please Verify this email.";
-                        mailOption.html = `<p>이 링크로 인증해주세요</p>`
-                    }
-                    await transporter.sendMail(mailOption,  (error,info): void => {
-                        if(error){ console.log(error); } else { console.log("Email is sent") }
-                        transporter.close();
-                    })
-                    const token1 = createJWT(user.id)
+                    const phoneVerification = await Verification.findOne({
+                        payload: args.phoneNumber,
+                        verified: true
+                    });
+                    if (phoneVerification) {
+                        const user = await User.create({...args}).save();
+                        const emailVerification = await Verification.create({
+                            payload: args.email,
+                            target: "EMAIL"
+                        });
+                        if(user.email){
+                            mailOption.to = user.email;
+                            mailOption.subject = "Please Verify this email.";
+                            mailOption.html = `<p>이 링크로 인증해주세요. 인증 코드는 ${emailVerification.key} 입니다.</p>`
+                        }
+                        await transporter.sendMail(mailOption,  (error,info): void => {
+                            if(error){ console.log(error); } else { console.log("Email is sent") }
+                            transporter.close();
+                        })
+                        const token1 = createJWT(user.id)
 
-                    return {
-                        ok: true,
-                        error: null,
-                        token: token1
-                    };
+                        return {
+                            ok: true,
+                            error: null,
+                            token: token1
+                        };
+                    } else {
+                        return {
+                            ok: false,
+                            error: "Please verify phoneNumber first",
+                            token: null
+                        };
+                    }
+
                 }
 
             } catch (error) {
